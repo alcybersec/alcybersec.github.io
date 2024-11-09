97bd149900b7820a055d8bd160639d6ba7b3e7ed
alcybersec <algitm@proton.me>
2024-11-09 13:48:09 +0400

    Personalisation

--- script.js
+++ script.js
@@ -14,3 +14,8 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+}
