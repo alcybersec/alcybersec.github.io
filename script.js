1803aa69a846a09d2a1447282a5fc5381f30cebf
alcybersec <algitm@proton.me>
2024-11-09 14:01:52 +0400

    Personalisation

--- script.js
+++ script.js
@@ -14,8 +14,3 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
-
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-}
