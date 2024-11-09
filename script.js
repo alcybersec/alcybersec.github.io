264127a14a55b0cd723c0e3b09940f8dfa74bd03
alcybersec <algitm@proton.me>
2024-11-09 20:20:35 +0400

    Updates

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const links = document.querySelectorAll(".nav a");
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".nav-links");
 
     links.forEach(link => {
         link.addEventListener("click", function(event) {
@@ -11,6 +13,22 @@ document.addEventListener("DOMContentLoaded", function() {
                 top: targetElement.offsetTop,
                 behavior: "smooth"
             });
+
+            // Close the menu after clicking a link
+            navLinks.classList.remove("active");
+            menuToggle.classList.remove("open");
+            menuToggle.textContent = "☰";
         });
+    });
+
+    menuToggle.addEventListener("click", function() {
+        navLinks.classList.toggle("active");
+        menuToggle.classList.toggle("open");
+
+        if (menuToggle.classList.contains("open")) {
+            menuToggle.textContent = "✖"; // Unicode for X
+        } else {
+            menuToggle.textContent = "☰";
+        }
     });
 });
